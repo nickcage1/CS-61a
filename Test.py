6c2a877cd57c827698ba689c1a5a5855b70563d5
unknown <channick314@gmail.com>
2022-11-09 11:52:17 -0800

    i commit

--- Test.py
+++ Test.py
@@ -1,11 +1,27 @@
-def stairs(n):
-    if n == 0:
-        return 1
-    elif n < 0:
-        return 0
-    else:
-        up_1 = stairs(n-1)
-        up_2 = stairs(n-2)
-        return up_1 + up_2
+def build_successors_table(tokens):
+    """Return a dictionary: keys are words; values are lists of successors.
 
-print (stairs(3))
+    >>> text = ['We', 'came', 'to', 'investigate', ',', 'catch', 'bad', 'guys', 'and', 'to', 'eat', 'pie', '.']
+    >>> table = build_successors_table(text)
+    >>> sorted(table)
+    [',', '.', 'We', 'and', 'bad', 'came', 'catch', 'eat', 'guys', 'investigate', 'pie', 'to']
+    >>> table['to']
+    ['investigate', 'eat']
+    >>> table['pie']
+    ['.']
+    >>> table['.']
+    ['We']
+    """
+    table = {}
+    prev = '.'
+    for word in tokens:
+        if prev not in table:
+            table[prev] = word
+        else:
+            table[prev] = word,table.get(prev)
+        prev = word
+    return table
+
+text = ['We', 'came', 'to', 'investigate', ',', 'catch', 'bad', 'guys', 'and', 'to', 'eat', 'pie', '.']
+table = build_successors_table(text)
+print (table['to'])
